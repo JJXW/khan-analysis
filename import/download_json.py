31bd265361a6228cab7f9f115222a2e160f98ea2
JJXW <jonathan352@gmail.com>
2015-06-01 18:39:01 -0700

    Pull from topic tree vs. playlists

--- import/download_json.py
+++ import/download_json.py
@@ -7,6 +7,13 @@ Created on Mon Jun 01 23:35:49 2015
 
 import requests
 import json
+import pandas
 
 test = json.loads(requests.get('http://www.khanacademy.org/api/v1/playlists').text)
+
+test_data_frame=pandas.io.json.json_normalize(test)
+
+test2 = json.loads(requests.get('http://www.khanacademy.org/api/v1/topictree').text)
+
+test_data_frame2=pandas.io.json.json_normalize(test)
 
